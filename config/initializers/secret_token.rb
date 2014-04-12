197055c068be90b4c6717095b9a61187b6101105
rurbanojimenez <r.urbanojimenez@gmail.com>
2014-04-12 15:03:49 +0200

    commit inicial con mongoid:config generado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = 'dbe0329bd98a69fb5ed4b84034961988ddc8003ca1967355b9bb234fcac5b64ff3b2675044514e0384602731b7958a4ca93c8e6530de44700c3b2ba510d17a69'
