47644628416ee31f92de1295bb08359de60cc378
rurbanojimenez <r.urbanojimenez@gmail.com>
2014-04-23 09:29:22 +0200

    me he colado y he creado el nuevo modelo event en esta rama...

---
+++ app/models/event.rb
@@ -0,0 +1,16 @@
+class Event
+  include Mongoid::Document
+
+  # Defino los fields
+  field :title, type: String
+  field :place, type: String
+  field :date, type: Date
+  field :desription, type: String, default: ""
+
+  # Restricciones...
+  validates_presence_of :title
+  validates_presence_of :date
+
+  # en rails 3 uso esto, en rails 4 usaré strong parameters
+  attr_accessible :title, :place, :date, :description
+end
