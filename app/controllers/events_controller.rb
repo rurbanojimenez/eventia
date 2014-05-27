c244429bafd9ead196e95e2205c99216b7d17e62
rurbano <r.urbanojimenez@gmail.com>
2014-05-27 08:15:08 +0200

    algunos cambios esteticos

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,7 +9,7 @@ class EventsController < ApplicationController
   	@event = current_user.mis_eventos.build(params[:event])
     if @event.save
       flash[:success] = "Event created!"
-      redirect_to root_url
+      redirect_to events_url
     else
       # @feed_items = []
       render "new"
@@ -29,7 +29,7 @@ class EventsController < ApplicationController
     @ev = Event.find(params[:id])
     @ev.destroy
     flash[:success] = "Event deleted."
-    redirect_to root_url
+    redirect_to events_url
   end
 
   #private
