35c70eaa1d50dcb62d36dc57b163b9ac77ddfe91
rurbanojimenez <r.urbanojimenez@gmail.com>
2014-05-27 12:56:28 +0200

    creados los botones follow y unfollow pero sin funcionalidad

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,3 +1,29 @@
 $(function(){
+	//paso los elementos a variables
+	var botonFollow = $("#botonFollow");
+	var botonUnfollow = $("#botonUnfollow");
+	var siguiendo = $("#invisible").text();
 	
+	//si sigo el evento, muestro unfollow, en caso contrario, muestro follow
+	if(siguiendo == "false"){
+		visibilidad(botonFollow,botonUnfollow);
+	}else{
+		visibilidad(botonUnfollow,botonFollow);
+	}
+
+	function visibilidad(elemVisible, elemInvisible){
+		elemVisible.removeClass("oculto");
+		elemInvisible.removeClass("visible-inline-block");
+		elemVisible.addClass("visible-inline-block");
+		elemInvisible.addClass("oculto");
+	}
+
+	//evento click en cada boton
+	botonFollow.click(function(){
+		visibilidad(botonUnfollow,botonFollow);
+	});
+	botonUnfollow.click(function(){
+		visibilidad(botonFollow,botonUnfollow);
+	});
+
 });
